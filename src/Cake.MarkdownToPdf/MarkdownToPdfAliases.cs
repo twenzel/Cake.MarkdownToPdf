8ce2294f7fc843936311f744f86f104c730f3dc9
Wenzel, Toni <toni.wenzel@googlemail.com>
2017-09-05 12:50:12 +0200

    Changed default font sizes

--- src/Cake.MarkdownToPdf/MarkdownToPdfAliases.cs
+++ src/Cake.MarkdownToPdf/MarkdownToPdfAliases.cs
@@ -150,63 +150,59 @@ namespace Cake.MarkdownToPdf
 
         private static void ApplyDefaultStyle(MigraDoc.DocumentObjectModel.Document document, MigraDoc.DocumentObjectModel.Section section)
         {
-            var normal = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Normal];
-            normal.Font = new MigraDoc.DocumentObjectModel.Font("Helvetica", MigraDoc.DocumentObjectModel.Unit.FromPoint(14));
+            var normalFontSize = 10; // 14 => 28,24,18,16,14,12  //10 => 20,18,14,12,10,8           
+            var paragraphMargin = 11;
 
+            var normal = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Normal];
+            normal.Font = new MigraDoc.DocumentObjectModel.Font("Helvetica", MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize));
 
             var heading1 = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Heading1];
-            heading1.Font = new MigraDoc.DocumentObjectModel.Font("Helvetica", MigraDoc.DocumentObjectModel.Unit.FromPoint(28));
+            heading1.Font = new MigraDoc.DocumentObjectModel.Font("Helvetica", MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize * 2));
             heading1.Font.Bold = true;
-            heading1.ParagraphFormat.SpaceAfter = MigraDoc.DocumentObjectModel.Unit.FromPoint(10);
-            heading1.ParagraphFormat.SpaceBefore = MigraDoc.DocumentObjectModel.Unit.FromPoint(20);
+            heading1.ParagraphFormat.SpaceAfter = MigraDoc.DocumentObjectModel.Unit.FromPoint(6);
+            heading1.ParagraphFormat.SpaceBefore = MigraDoc.DocumentObjectModel.Unit.FromPoint(12);
 
             var heading2 = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Heading2];
             heading2.BaseStyle = FSharp.Markdown.Pdf.MarkdownStyleNames.Heading1;
-            heading2.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(24);
+            heading2.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize + 8);
             heading2.ParagraphFormat.Borders.Bottom.Color = new MigraDoc.DocumentObjectModel.Color(204, 204, 204);
             heading2.ParagraphFormat.Borders.Bottom.Width = MigraDoc.DocumentObjectModel.Unit.FromPoint(1);
 
             var heading3 = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Heading3];
             heading3.BaseStyle = FSharp.Markdown.Pdf.MarkdownStyleNames.Heading1;
-            heading3.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(18);
+            heading3.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize + 4);
 
             var heading4 = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Heading4];
             heading4.BaseStyle = FSharp.Markdown.Pdf.MarkdownStyleNames.Heading1;
-            heading4.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(16);
+            heading4.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize + 2);
 
             var heading5 = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Heading5];
             heading5.BaseStyle = FSharp.Markdown.Pdf.MarkdownStyleNames.Heading1;
-            heading5.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(14);
+            heading5.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize);
 
             var heading6 = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Heading6];
             heading6.BaseStyle = FSharp.Markdown.Pdf.MarkdownStyleNames.Heading1;
-            heading6.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(12);
+            heading6.Font.Size = MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize - 2);
 
             var code = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Code];
-            code.Font = new MigraDoc.DocumentObjectModel.Font("Consolas", MigraDoc.DocumentObjectModel.Unit.FromPoint(12));
+            code.Font = new MigraDoc.DocumentObjectModel.Font("Consolas", MigraDoc.DocumentObjectModel.Unit.FromPoint(normalFontSize - 2));
             code.ParagraphFormat.Borders.Color = new MigraDoc.DocumentObjectModel.Color(234, 234, 234);
-            //code.ParagraphFormat.Borders.Bottom.Width = MigraDoc.DocumentObjectModel.Unit.FromPoint(1);
-            //code.ParagraphFormat.Borders.Bottom.Style = MigraDoc.DocumentObjectModel.BorderStyle.Single;
-            //code.ParagraphFormat.Borders.Bottom.Visible = true;
-            //code.ParagraphFormat.Borders.Top.Width = MigraDoc.DocumentObjectModel.Unit.FromPoint(1);
-            //code.ParagraphFormat.Borders.Left.Width = MigraDoc.DocumentObjectModel.Unit.FromPoint(1);
-            //code.ParagraphFormat.Borders.Right.Width = MigraDoc.DocumentObjectModel.Unit.FromPoint(1);
             code.ParagraphFormat.Borders.Visible = true;
             code.ParagraphFormat.Borders.Style = MigraDoc.DocumentObjectModel.BorderStyle.Single;
             code.ParagraphFormat.Borders.Width = MigraDoc.DocumentObjectModel.Unit.FromPoint(1);
 
             code.ParagraphFormat.Shading.Color = new MigraDoc.DocumentObjectModel.Color(248, 248, 248);
             code.ParagraphFormat.Shading.Visible = true;
-            code.ParagraphFormat.SpaceAfter = MigraDoc.DocumentObjectModel.Unit.FromPoint(15);
-            code.ParagraphFormat.SpaceBefore = MigraDoc.DocumentObjectModel.Unit.FromPoint(15);
+            code.ParagraphFormat.SpaceAfter = MigraDoc.DocumentObjectModel.Unit.FromPoint(paragraphMargin);
+            code.ParagraphFormat.SpaceBefore = MigraDoc.DocumentObjectModel.Unit.FromPoint(paragraphMargin);
 
             var hyperlink = document.Styles[FSharp.Markdown.Pdf.MarkdownStyleNames.Hyperlink];
             hyperlink.Font.Color = new MigraDoc.DocumentObjectModel.Color(65, 131, 196);
             hyperlink.Font.Underline = MigraDoc.DocumentObjectModel.Underline.None;
 
             var p = document.AddStyle("p", FSharp.Markdown.Pdf.MarkdownStyleNames.Normal);
-            p.ParagraphFormat.SpaceAfter = MigraDoc.DocumentObjectModel.Unit.FromPoint(15);
-            p.ParagraphFormat.SpaceBefore = MigraDoc.DocumentObjectModel.Unit.FromPoint(15);
+            p.ParagraphFormat.SpaceAfter = MigraDoc.DocumentObjectModel.Unit.FromPoint(paragraphMargin);
+            p.ParagraphFormat.SpaceBefore = MigraDoc.DocumentObjectModel.Unit.FromPoint(paragraphMargin);
 
             foreach (var element in section.Elements)
             {
