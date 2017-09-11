f9c8a1fb2f2f6383d5d3a0d376a387b5af15588e
Wenzel, Toni <toni.wenzel@googlemail.com>
2017-09-11 14:46:35 +0200

    Add test

---
+++ src/Cake.MarkdownToPdf.Tests/Assets/TestImage.md
@@ -0,0 +1,9 @@
+implicit
+
+![Possible topics for crosscutting
+concepts](images/08-Crosscutting-Concepts-Structure-DE.png)
+
+test 2
+![Possible topics for crosscutting
+concepts](./images/08-Crosscutting-Concepts-Structure-DE.png)
+
--- src/Cake.MarkdownToPdf.Tests/MarkdownFileToPdfTests.cs
+++ src/Cake.MarkdownToPdf.Tests/MarkdownFileToPdfTests.cs
@@ -1,6 +1,7 @@
 using Cake.Core;
 using NSubstitute;
 using Xunit;
+using Cake.Core.Diagnostics;
 
 namespace Cake.MarkdownToPdf.Tests
 {
@@ -10,11 +11,28 @@ namespace Cake.MarkdownToPdf.Tests
         public void ConvertFile()
         {
             ICakeContext context = Substitute.For<ICakeContext>();
+           
+            //context.When(c => c.Log.Write(Arg.Any<Core.Diagnostics.Verbosity>(), Core.Diagnostics.LogLevel.Error, Arg.Any<string>(), Arg.Any<object[]>())).Do(call =>
+            //Assert.False(true, call.Arg<string>())
+            //);
 
-            MarkdownToPdfAliases.MarkdownFileToPdf(context, "Assets/Test.md", "Testoutput.pdf", settings => {
+            MarkdownToPdfAliases.MarkdownFileToPdf(context, "Assets/Test.md", "Testoutput.pdf", settings =>
+            {
                 settings.UseAdvancedMarkdownTables();
                 settings.Theme = Themes.Github;
             });
+        }
+
+        [Fact]
+        public void OutputImages()
+        {
+            ICakeContext context = Substitute.For<ICakeContext>();
+            //context.Log.Information("test");
+
+            //context.When(c => c.Log.Write(Arg.Any<Core.Diagnostics.Verbosity>(), Core.Diagnostics.LogLevel.Error, Arg.Any<string>(), Arg.Any<object[]>()))
+            //    .Do(call => Assert.False(true, call.Arg<string>()));
+
+            MarkdownToPdfAliases.MarkdownFileToPdf(context, "Assets/TestImage.md", "TestImage.pdf");
         }
     }
 }
